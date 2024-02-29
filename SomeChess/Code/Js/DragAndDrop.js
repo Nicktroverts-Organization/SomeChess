c5caef74f5fff4bf077518998ec4a72870579bed
1ilkin <ilkin.chess1@gmail.com>
2024-02-29 11:38:23 +0100

    drag and drop doen't work and I don't even know why!! HELP!! NICK HELP!!!

---
+++ SomeChess/Code/Js/DragAndDrop.js
@@ -0,0 +1,19 @@
+﻿window.BlazorDragAndDrop = {
+    DragStart: function (e) {
+        e.preventDefault();
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/plain', e.target.id);
+    },
+    DragEnd: function (e) {
+        e.preventDefault();
+    },
+    DragOver: function (e) {
+        e.preventDefault();
+    },
+    Drop: function (e) {
+        e.preventDefault();
+        var data = e.dataTransfer.getData('text/plain');
+        var draggedElement = document.getElementById(data);
+        e.target.appendChild(draggedElement);
+    }
+};
